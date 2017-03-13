31174d300e0833faf35d51df543609348b4bde22
wapjude <wapjude@gmail.com>
2017-03-13 14:30:48 +0000

    Add Formatting to get Primes

--- app/index.js
+++ app/index.js
@@ -3,42 +3,77 @@ module.exports = {
         if(data === null || typeof data === 'undefined'){
             return 'no value';
         }else if(typeof data === 'boolean'){
+
             return data;
+
         }else if(typeof data === 'number'){
+
             if(data < 100){
+
                 return 'less than 100';
+
             }else{
+
                 return data === 100? 'equal to 100' : 'more than 100';
+
             }
+
         }else if(typeof data === "string"){
+
             return data.length;
+
         }else if(typeof data === "object" && Array.isArray(data)){
+
             return data.length >= 3 ? data[2] : undefined;
+
         }else if(typeof data === "function"){
+            
             return data(true);
+        
         }
     },
     getPrimes:function(num){
         if(typeof num !== 'number'){
+        
             return 'invalid input';
+        
         }else if(num === 0){
+        
             return 'zero supplied as input';
+        
         }else if(num === 1){
+        
         return 'not prime';
+        
         }else if(num < 0){
+        
             return 'negative value supplied';
+        
         }else{
+        
             var prime = [];
+        
             for(var i = 2; i<= num; i++){
+        
                 var is_prime = true;
+        
               for(var j = 2; j< i; j++){
+        
                   if( i % j === 0){
+        
                       is_prime = false;
+        
                   }
+        
               }  
+        
                if(is_prime){
+        
                   prime.push(i); 
+        
+
                };
+        
             }
             return prime;
         }
